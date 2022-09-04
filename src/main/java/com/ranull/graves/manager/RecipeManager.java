e979ab6f36ea2d1daaf2c907196388597ab2a248
deiphiz <gameguy245@gmail.com>
2022-09-04 07:19:38 -0700

    nvm let's start from the old version instead lol

--- src/main/java/com/ranull/graves/manager/RecipeManager.java
+++ src/main/java/com/ranull/graves/manager/RecipeManager.java
@@ -1,230 +1,99 @@
 package com.ranull.graves.manager;
 
 import com.ranull.graves.Graves;
-import com.ranull.graves.util.StringUtil;
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
-import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.Recipe;
 import org.bukkit.inventory.ShapedRecipe;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 
-public final class RecipeManager {
-    private final Graves plugin;
-    private final List<NamespacedKey> namespacedKeyList;
+public class RecipeManager {
+    private Graves plugin;
+    private GraveManager graveManager;
 
-    public RecipeManager(Graves plugin) {
+    public RecipeManager(Graves plugin, GraveManager graveManager) {
         this.plugin = plugin;
-        this.namespacedKeyList = new ArrayList<>();
-        reload();
+        this.graveManager = graveManager;
     }
 
-    public void reload() {
-        unload();
-        load();
+    public void loadRecipes() {
+        if (plugin.getConfig().getBoolean("settings.token") &&
+                plugin.getConfig().getBoolean("settings.tokenRecipeEnabled")) {
+            graveTokenRecipe();
+        }
     }
 
-    public void load() {
-        ConfigurationSection configurationSection = plugin.getConfig().getConfigurationSection("settings.token");
-
-        if (configurationSection != null) {
-            for (String key : configurationSection.getKeys(false)) {
-                if (plugin.getConfig().getBoolean("settings.token." + key + ".craft")) {
-                    addTokenRecipe(key, getToken(key));
-                    plugin.debugMessage("Added recipe " + key, 1);
+    public void unloadRecipes() {
+        Material graveTokenMaterial = Material.matchMaterial(Objects.requireNonNull(plugin.getConfig().getString("settings.tokenItem")));
+        if (graveTokenMaterial != null) {
+            Iterator<Recipe> recipes = plugin.getServer().recipeIterator();
+            while (recipes.hasNext()) {
+                Recipe recipe = recipes.next();
+                if (recipe != null) {
+                    ItemStack itemStack = recipe.getResult();
+                    if (itemStack.hasItemMeta()) {
+                        if (hasRecipeData(itemStack)) {
+                            recipes.remove();
+                        }
+                    }
                 }
             }
         }
     }
 
-    public void unload() {
-        Iterator<Recipe> iterator = plugin.getServer().recipeIterator();
+    public void graveTokenRecipe() {
+        Material graveTokenMaterial = Material.matchMaterial(Objects.requireNonNull(plugin.getConfig().getString("settings.tokenItem")));
 
-        while (iterator.hasNext()) {
-            Recipe recipe = iterator.next();
+        if (graveTokenMaterial != null) {
+            ItemStack item = graveManager.getGraveToken();
 
-            if (recipe != null) {
-                ItemStack itemStack = recipe.getResult();
+            NamespacedKey key = new NamespacedKey(plugin, "grave_token");
+            ShapedRecipe recipe = new ShapedRecipe(key, item);
 
-                if (itemStack.hasItemMeta() && isToken(itemStack)) {
-                    iterator.remove();
-                }
-            }
-        }
-    }
+            recipe.shape("ABC", "DEF", "GHI");
 
-    public ItemStack getToken(String token) {
-        if (plugin.getConfig().isConfigurationSection("settings.token." + token)) {
-            Material material = Material.matchMaterial(plugin.getConfig()
-                    .getString("settings.token." + token + ".material", "SUNFLOWER"));
-            ItemStack itemStack = new ItemStack(material != null ? material : Material.CHEST);
+            List<String> lines = plugin.getConfig().getStringList("settings.tokenRecipe");
+            Integer recipeKey = 1;
 
-            setRecipeData(token, itemStack);
-
-            if (itemStack.hasItemMeta()) {
-                ItemMeta itemMeta = itemStack.getItemMeta();
-
-                if (itemMeta != null) {
-                    String name = ChatColor.WHITE + StringUtil.parseString(plugin.getConfig()
-                            .getString("settings.token." + token + ".name"), plugin);
-                    List<String> loreList = new ArrayList<>();
-                    int customModelData = plugin.getConfig().getInt("settings.token." + token
-                            + ".model-data", -1);
-
-                    for (String string : plugin.getConfig().getStringList("settings.token." + token + ".lore")) {
-                        loreList.add(ChatColor.GRAY + StringUtil.parseString(string, plugin));
-                    }
-
-                    if (plugin.getConfig().getBoolean("settings.token." + token + ".glow")) {
-                        itemMeta.addEnchant(Enchantment.DURABILITY, 1, true);
-                        itemMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-                    }
-
-                    if (customModelData > -1) {
-                        itemMeta.setCustomModelData(customModelData);
-                    }
-
-                    itemMeta.setLore(loreList);
-                    itemMeta.setDisplayName(name);
-                    itemStack.setItemMeta(itemMeta);
-                }
-            }
-
-            return itemStack;
-        }
-
-        return null;
-    }
-
-    public List<String> getTokenList() {
-        List<String> stringList = new ArrayList<>();
-        ConfigurationSection configurationSection = plugin.getConfig().getConfigurationSection("settings.token");
-
-        if (configurationSection != null) {
-            stringList.addAll(configurationSection.getKeys(false));
-        }
-
-        return stringList;
-    }
-
-    public void addTokenRecipe(String token, ItemStack itemStack) {
-        NamespacedKey namespacedKey = new NamespacedKey(plugin, token + "GraveToken");
-
-        if (!namespacedKeyList.contains(namespacedKey)) {
-            ShapedRecipe shapedRecipe = new ShapedRecipe(namespacedKey, itemStack);
-
-            shapedRecipe.shape("ABC", "DEF", "GHI");
-
-            List<String> lineList = plugin.getConfig().getStringList("settings.token." + token + ".recipe");
-            int recipeKey = 1;
-
-            for (String string : lineList.get(0).split(" ")) {
+            for (String string : lines.get(0).split(" ")) {
                 Material material = Material.matchMaterial(string);
-
                 if (material != null) {
-                    shapedRecipe.setIngredient(getChar(recipeKey), material);
+                    recipe.setIngredient(getChar(recipeKey), material);
                 }
-
                 recipeKey++;
             }
 
-            for (String string : lineList.get(1).split(" ")) {
+            for (String string : lines.get(1).split(" ")) {
                 Material material = Material.matchMaterial(string);
-
                 if (material != null) {
-                    shapedRecipe.setIngredient(getChar(recipeKey), material);
+                    recipe.setIngredient(getChar(recipeKey), material);
                 }
-
                 recipeKey++;
             }
 
-            for (String string : lineList.get(2).split(" ")) {
+            for (String string : lines.get(2).split(" ")) {
                 Material material = Material.matchMaterial(string);
-
                 if (material != null) {
-                    shapedRecipe.setIngredient(getChar(recipeKey), material);
+                    recipe.setIngredient(getChar(recipeKey), material);
                 }
-
                 recipeKey++;
             }
 
-
-            if (plugin.getServer().getRecipe(namespacedKey) == null) {
-                plugin.getServer().addRecipe(shapedRecipe);
-                namespacedKeyList.add(namespacedKey);
-            } else {
-                plugin.debugMessage("Unable to add recipe " + namespacedKey.getKey(), 1);
-            }
+            plugin.getServer().addRecipe(recipe);
         }
     }
 
-    public ItemStack getGraveTokenFromPlayer(String token, List<ItemStack> itemStackList) {
-        for (ItemStack itemStack : itemStackList) {
-            if (itemStack != null && isToken(token, itemStack)) {
-                return itemStack;
-            }
-        }
-
-        return null;
+    public boolean hasRecipeData(ItemStack item) {
+        return Objects.requireNonNull(item.getItemMeta()).getPersistentDataContainer()
+                .has(new NamespacedKey(plugin, "gravesRecipe"), PersistentDataType.INTEGER);
     }
 
-    public void setRecipeData(String token, ItemStack itemStack) {
-        if (plugin.getVersionManager().hasPersistentData()) {
-            ItemMeta itemMeta = itemStack.getItemMeta();
-
-            if (itemMeta != null) {
-                itemMeta.getPersistentDataContainer().set(new NamespacedKey(plugin, "token"),
-                        PersistentDataType.STRING, token);
-                itemStack.setItemMeta(itemMeta);
-            }
-        }
-    }
-
-    public boolean isToken(String token, ItemStack itemStack) {
-        if (plugin.getVersionManager().hasPersistentData()) {
-            if (itemStack.getItemMeta() != null && itemStack.getItemMeta().getPersistentDataContainer()
-                    .has(new NamespacedKey(plugin, "token"), PersistentDataType.STRING)) {
-                String string = itemStack.getItemMeta().getPersistentDataContainer()
-                        .get(new NamespacedKey(plugin, "token"), PersistentDataType.STRING);
-
-                return string != null && string.equals(token);
-            }
-        } else {
-            // TODO
-            return false;
-        }
-
-        return false;
-    }
-
-    public String getTokenName(ItemStack itemStack) {
-        if (plugin.getVersionManager().hasPersistentData()) {
-            if (itemStack.getItemMeta() != null && itemStack.getItemMeta().getPersistentDataContainer()
-                    .has(new NamespacedKey(plugin, "token"), PersistentDataType.STRING)) {
-                return itemStack.getItemMeta().getPersistentDataContainer()
-                        .get(new NamespacedKey(plugin, "token"), PersistentDataType.STRING);
-            }
-        }
-
-        return null;
-    }
-
-    public boolean isToken(ItemStack itemStack) {
-        return plugin.getVersionManager().hasPersistentData() && itemStack.getItemMeta() != null
-                && itemStack.getItemMeta().getPersistentDataContainer()
-                .has(new NamespacedKey(plugin, "token"), PersistentDataType.STRING);
-    }
-
-    private char getChar(int count) {
+    public char getChar(Integer count) {
         switch (count) {
             case 1:
                 return 'A';
